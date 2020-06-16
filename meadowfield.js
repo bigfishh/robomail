9a0b03f370567f2996204d87e60acb2403741aee
bigfishh <anniezheng.anniee@gmail.com>
2020-06-16 13:56:58 -0400

    added robot direction

--- meadowfield.js
+++ meadowfield.js
@@ -49,8 +49,6 @@ class VillageState {
                 return this;
         } else {
             let parcels = this.parcels.map(p => {
-                console.log( "p", p)
-                console.log( "this.place", this.place)
                 if (p.place != this.place) {
                     return p
                 }
@@ -63,13 +61,43 @@ class VillageState {
 
 }
 
-let first = new VillageState (
-    "Post Office",
-    [{place: "Post Office", address: "Alice's House"}]
-);
+function runRobot(state, robot, memory) {
+    for (let turn = 0;; turn++) {
 
-let next = first.move("Grete's House");
-console.log(next.place); // → Alice's House 
-console.log(next.parcels); // → [] 
-console.log(first.place); // → Post Office
+        if (state.parcels.length === 0) {
+            console.log(`Done in ${turn} turns`)
+            break
+        }
 
+        let action = robot(state, memory) 
+        state = state.move(action.direction)
+        memory = action.memory
+        console.log(`Moved to ${action.direction}`)
+        
+    }
+
+}
+
+function randomPick(array) {
+    let choice = Math.floor(Math.random() * array.length)
+    return array[choice]
+}
+
+function randomRobot(state) {
+    return {direction: randomPick(roadGraph[state.place])}
+}
+
+VillageState.random = function(parcelCount = 5) {
+    let parcels = []
+    for (let i = 0; i < parcelCount; i++) {
+        let address = randomPick(Object.keys(roadGraph))
+        let place
+        do {
+            place = randomPick(Object.keys(roadGraph))
+        } while (place === address) 
+        parcels.push({place, address})
+    }
+    return new VillageState("Post Office", parcels)
+}
+
+console.log(runRobot(VillageState.random(), randomRobot))
